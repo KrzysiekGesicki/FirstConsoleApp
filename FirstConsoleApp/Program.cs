e52c140ad2a0cc9f062d8cfeb0478cf3c258430f
Krzysiek Gesicki <krzysiek.gesicki@gmail.com>
2023-01-20 11:51:45 +0100

    added some examples

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -10,6 +10,35 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
+            string name1 = "Marek";
+            string name2 = "Ania";
+
+            //komentarz jednolinijkowy
+
+            /*komenatrz dlugi
+            */
+
+            Console.WriteLine(name1);
+
+            Console.WriteLine("Imie nr 1 to: {0}", name1);
+
+            Console.WriteLine($"Imie nr 1 to: {name1}");
+
+            Console.WriteLine("Tekst i cudzyslow w srodku: \"tak jak tutaj\" ");
+
+            Console.ReadLine();
+
+            double x = 2.3;
+            float y = 1.1f;
+            float z = 3.9F;
+            Console.WriteLine("wartosc x= {0}", x);
+            Console.WriteLine("wartosc y= {0}", y);
+            Console.WriteLine("wartosc z= {0}", z);
+
+            string s = Convert.ToString(z);
+            Console.WriteLine("wartosc s= {0}", s);
+
+            Console.ReadLine();
         }
     }
 }
